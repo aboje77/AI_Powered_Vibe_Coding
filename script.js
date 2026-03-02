e5167602ffc91fa85fdbf7f2b3c3a14729e02f65
Aboje Ebute <abojeebute1@gmail.com>
2026-03-02 22:19:52 +0100

    AI_Powered_Coding

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+  File: script.js
+  Description: Adds interactivity to landing page
+  Author: Your Name
+  Date: 2026
+*/
+
+// Select button
+const projectBtn = document.getElementById("projectBtn");
+
+// Add click event
+projectBtn.addEventListener("click", function() {
+    alert("Projects section coming soon!");
+});
